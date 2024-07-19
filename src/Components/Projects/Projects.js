495db8a060372281bf2cc713d0a7aba43b77a392
Marwan-Tamer777 <marwanmarwan536@gmail.com>
2024-07-19 06:05:18 +0300

    updated project card

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -26,11 +26,13 @@ function Projects() {
         and hosted active projects will be listed below
       </p>
 
-      <div className={`flex flex-row justify-between p-5`}>
+      <div
+        className={`flex flex-row flex-wrap justify-between p-5 text-center`}
+      >
         <a
           href="https://udacity-mimic.netlify.app"
           target={"_blank"}
-          className="w-[45%] scale-up"
+          className="w-[45%] scale-up my-4"
         >
           <div className={`flex flex-col rounded-xl ${colors.cardBg}`}>
             <img
@@ -55,7 +57,7 @@ function Projects() {
         <a
           href="https://tech-talks-mt.netlify.app/"
           target={"_blank"}
-          className="w-[45%] scale-up"
+          className="w-[45%] scale-up my-4"
         >
           <div className={`flex flex-col rounded-xl ${colors.cardBg}`}>
             <img
@@ -67,13 +69,11 @@ function Projects() {
               A Podcast Website!
             </p>
             <p className={` text-center ${colors.secText} capitalize p-4`}>
-              This project was a submission for "Fundamentals of Management"
-              subject at My University. It is build on vanilla html, css,js. and
-              has a server that uses spotify free API to embed top podcasts into
-              the website (Due to spotify terms, a website can't use their Full
-              API for free, so this website version doesn't use the embeds
-              feature. but you can download the source code from my github and
-              update the server to use your own API keys to try it yourself!)
+              This project was a submission for MyUniversity. It is a vanilla
+              website with a server that uses spotify API to embed podcasts into
+              the website (Due to spotify terms, the website can't use their
+              Full API for free. but you can download the source code to check
+              the server and use your own API keys to try it yourself!)
             </p>
           </div>
         </a>
@@ -81,7 +81,7 @@ function Projects() {
         <a
           href="https://myreads-marwan.netlify.app/"
           target={"_blank"}
-          className="w-[45%] scale-up"
+          className="w-[45%] scale-up my-4"
         >
           <div className={`flex flex-col rounded-xl ${colors.cardBg}`}>
             <img
@@ -93,9 +93,9 @@ function Projects() {
               A book search Website!
             </p>
             <p className={` text-center ${colors.secText} capitalize p-4`}>
-              This project was a submission for Udacity's front-end professional nanodegree.
-              It is a simple book tracking app that you can add your books on it and search for new books
-              using a third party API
+              This project was a submission for Udacity's front-end professional
+              nanodegree. It is a simple book tracking app that you can add your
+              books on it and search for new books using a third party API
             </p>
           </div>
         </a>
